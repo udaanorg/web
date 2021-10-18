cf106bda0e51f655b7a3e977f73cf5981e0bd9e7
Shraddha <shraddha3m@gmail.com>
2021-10-18 22:58:06 +0530

    test styling

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import "../styles/login.css";
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
